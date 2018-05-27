3be84423081dd24e8dd80f24e9c1cb0d24ef8239
suganthy <suganthy876@gmail.com>
2018-05-28 01:02:05 +0530

    Scss to css changes.

--- src/app/common/common.component.ts
+++ src/app/common/common.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-common',
   templateUrl: './common.component.html',
-  styleUrls: ['./common.component.scss']
+  styleUrls: ['./common.component.css']
 })
 export class CommonComponent implements OnInit {
 
